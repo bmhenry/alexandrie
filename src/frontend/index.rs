dbee6b161b6bf05b59811e124f31346a520ac94a
Brandon Henry <brandon@bhenry.dev>
2020-03-29 11:28:59 -0400

    front page done for the moment

--- src/frontend/index.rs
+++ src/frontend/index.rs
@@ -8,6 +8,7 @@ use crate::db::schema::*;
 use crate::db::DATETIME_FORMAT;
 use crate::error::Error;
 use crate::frontend::helpers;
+use crate::index::Indexer;
 use crate::utils;
 use crate::utils::auth::AuthExt;
 use crate::State;
@@ -36,17 +37,17 @@ pub(crate) async fn get(req: Request<State>) -> Result<Response, Error> {
 
         //? Get the 10 most downloaded crates.
         let most_downloaded = crates::table
-            .select((crates::name, crates::downloads))
+            .select((crates::name, crates::description, crates::downloads))
             .order_by(crates::downloads.desc())
             .limit(10)
-            .load::<(String, i64)>(conn)?;
+            .load::<(String, Option<String>, i64)>(conn)?;
 
         //? Get the 10 most recently updated crates.
         let last_updated = crates::table
-            .select((crates::name, crates::updated_at))
+            .select((crates::name, crates::description, crates::updated_at))
             .order_by(crates::updated_at.desc())
             .limit(10)
-            .load::<(String, String)>(conn)?;
+            .load::<(String, Option<String>, String)>(conn)?;
 
         let engine = &state.frontend.handlebars;
         let context = json!({
@@ -54,15 +55,19 @@ pub(crate) async fn get(req: Request<State>) -> Result<Response, Error> {
             "instance": &state.frontend.config,
             "total_downloads": helpers::humanize_number(total_downloads),
             "crate_count": helpers::humanize_number(crate_count),
-            "most_downloaded": most_downloaded.into_iter().map(|(name, downloads)| json!({
+            "index_url": state.index.url().as_mut().map(|url| url.trim()).ok(),
+            "most_downloaded": most_downloaded.into_iter().map(|(name, description, downloads)| json!({
                 "name": name,
+                "version": state.index.latest_record(&name).map(|rec| rec.vers).ok(),
+                "description": description,
                 "downloads": helpers::humanize_number(downloads),
             })).collect::<Vec<_>>(),
-            "last_updated": last_updated.into_iter().map(|(name, date)| {
-                let updated_at = chrono::NaiveDateTime::parse_from_str(date.as_str(), DATETIME_FORMAT).unwrap();
+            "last_updated": last_updated.into_iter().map(|(name, description, date)| {
+                let updated_at = chrono::NaiveDate::parse_from_str(date.as_str(), DATETIME_FORMAT).unwrap();
                 json!({
                     "name": name,
-                    "updated_at": helpers::humanize_datetime(updated_at),
+                    "description": description,
+                    "updated_at": helpers::humanize_date(updated_at),
                 })
             }).collect::<Vec<_>>(),
         });
